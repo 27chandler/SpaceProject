f2fd234b2d806575c2a5b42dd3034c47411e9ddf
27chandler <chandler27s@yahoo.com>
2020-01-15 17:10:38 +0000

    Added ship system and movement

--- Exploration_Game/Assets/Assets/Movement.cs
+++ Exploration_Game/Assets/Assets/Movement.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private Tilemap ship_floor_tilemap;
+
     private Rigidbody2D rb;
 
-    [SerializeField] private float movement_speed, rotation_speed;
+    private Rigidbody2D parent_rigidbody;
+
+    private Vector2 last_frame_anchor;
+    private Vector2 ship_anchor;
+    private bool is_anchor_point_set = false;
+    private bool on_ship_floor = false;
+
+    private float current_rotation = 0.0f;
+
+    [SerializeField] private float movement_speed, rotation_speed, drag_force, relative_constant;
     [Space]
     [SerializeField] private KeyCode up_key;
     [SerializeField] private KeyCode down_key;
@@ -28,22 +40,22 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(up_key))
         {
-            rb.AddForce(transform.up * movement_speed);
+            rb.AddRelativeForce(Vector3.up * movement_speed);
         }
 
         if (Input.GetKey(down_key))
         {
-            rb.AddForce((-transform.up) * movement_speed);
+            rb.AddRelativeForce((-Vector3.up) * movement_speed);
         }
 
         if (Input.GetKey(left_key))
         {
-            rb.AddForce((-transform.right) * movement_speed);
+            rb.AddRelativeForce((-Vector3.right) * movement_speed);
         }
 
         if (Input.GetKey(right_key))
         {
-            rb.AddForce(transform.right * movement_speed);
+            rb.AddRelativeForce(Vector3.right * movement_speed);
         }
 
         //
@@ -51,11 +63,75 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(clockwise_key))
         {
             rb.AddTorque(-rotation_speed);
+            current_rotation -= rotation_speed;
         }
 
         if (Input.GetKey(anticlockwise_key))
         {
             rb.AddTorque(rotation_speed);
+            current_rotation += rotation_speed;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        if ((parent_rigidbody == null) || (!on_ship_floor))
+        {
+            rb.AddForce(-(rb.velocity * drag_force));
+        }
+
+        if (parent_rigidbody != null)
+        {
+            Vector3Int ship_floor_pos = new Vector3Int();
+            Vector2 conversion_pos = parent_rigidbody.GetPoint(new Vector2(transform.position.x, transform.position.y));
+            ship_floor_pos.x = Mathf.FloorToInt(conversion_pos.x);
+            ship_floor_pos.y = Mathf.FloorToInt(conversion_pos.y);
+
+            if (ship_floor_tilemap.GetTile(ship_floor_pos) != null)
+            {
+                on_ship_floor = true;
+            }
+            else
+            {
+                on_ship_floor = false;
+            }
+
+            if (on_ship_floor)
+            {
+                if (last_frame_anchor != parent_rigidbody.GetRelativePoint(ship_anchor))
+                {
+                    if (is_anchor_point_set)
+                    {
+                        rb.velocity = ((parent_rigidbody.GetRelativePoint(ship_anchor) - last_frame_anchor) * 50.0f);
+                        rb.angularVelocity = parent_rigidbody.angularVelocity;
+                    }
+                    else
+                    {
+                        is_anchor_point_set = true;
+                    }
+
+                    last_frame_anchor = parent_rigidbody.GetRelativePoint(ship_anchor);
+                }
+
+                if (Vector2.Distance(parent_rigidbody.GetRelativePoint(ship_anchor), transform.position) > 0.1f)
+                {
+                    Set_Anchor_Point();
+                    is_anchor_point_set = false;
+                    Debug.Log("Re-Anchored");
+                }
+            }
+        }
+    }
+
+    public void Set_Parent(Rigidbody2D i_rb)
+    {
+        parent_rigidbody = i_rb;
+        last_frame_anchor = parent_rigidbody.GetRelativePoint(ship_anchor);
+        Set_Anchor_Point();
+    }
+
+    private void Set_Anchor_Point()
+    {
+        ship_anchor = parent_rigidbody.GetPoint(rb.position);
     }
 }
---
+++ Exploration_Game/Assets/Assets/Systems/Ship_System.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Ship_System : Tile_System
+{
+    [SerializeField] private Rigidbody2D ship_rb;
+    [SerializeField] private Movement movement;
+
+    [SerializeField] private List<Tilemap> tilemaps = new List<Tilemap>();
+    [SerializeField] private List<Tilemap> ship_tilemaps = new List<Tilemap>();
+    [SerializeField] private Transform ship_grid;
+
+    private bool is_converting_to_ship = false;
+
+    private Dictionary<Vector3Int,bool> ship_tile_positions = new Dictionary<Vector3Int, bool>();
+
+    private void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            is_converting_to_ship = !is_converting_to_ship;
+        }
+
+        if (is_converting_to_ship)
+        {
+            Convert_To_Ship(new Vector3Int(-5,23,0));
+            Copy_Over(0);
+            Copy_Touching_Layers(0);
+            
+            movement.Set_Parent(ship_rb);
+
+            is_converting_to_ship = false;
+        }
+    }
+
+    private void Convert_To_Ship(Vector3Int i_start_pos)
+    {
+        ship_tile_positions.Add(i_start_pos,true);
+
+        bool is_conversion_finished = false;
+        int tilemap_index = 0;
+        while (!is_conversion_finished)
+        {
+            List<Vector3Int> edge_tiles = new List<Vector3Int>();
+
+            foreach (var pos in ship_tile_positions)
+            {
+                
+                Vector3Int check_pos = pos.Key + new Vector3Int(1, 0, 0);
+                if ((!ship_tile_positions.ContainsKey(check_pos)) && (tilemaps[tilemap_index].GetTile(check_pos) != null))
+                {
+                    edge_tiles.Add(check_pos);
+                }
+
+                check_pos = pos.Key + new Vector3Int(-1, 0, 0);
+                if ((!ship_tile_positions.ContainsKey(check_pos)) && (tilemaps[tilemap_index].GetTile(check_pos) != null))
+                {
+                    edge_tiles.Add(check_pos);
+                }
+
+                check_pos = pos.Key + new Vector3Int(0, 1, 0);
+                if ((!ship_tile_positions.ContainsKey(check_pos)) && (tilemaps[tilemap_index].GetTile(check_pos) != null))
+                {
+                    edge_tiles.Add(check_pos);
+                }
+
+                check_pos = pos.Key + new Vector3Int(0, -1, 0);
+                if ((!ship_tile_positions.ContainsKey(check_pos)) && (tilemaps[tilemap_index].GetTile(check_pos) != null))
+                {
+                    edge_tiles.Add(check_pos);
+                }
+            }
+
+            if (edge_tiles.Count == 0)
+            {
+                is_conversion_finished = true;
+            }
+
+            foreach (var tile_pos in edge_tiles)
+            {
+                if (!ship_tile_positions.ContainsKey(tile_pos))
+                {
+                    ship_tile_positions.Add(tile_pos, true);
+                }
+            }
+            edge_tiles.Clear();
+
+        }
+
+        Debug.Log("Conversion finished with " + ship_tile_positions.Count + " tiles found");
+    }
+
+    private void Copy_Over(int i_index)
+    {
+        foreach (var tile in ship_tile_positions)
+        {
+            ship_tilemaps[i_index].SetTile(tile.Key, tilemaps[i_index].GetTile(tile.Key));
+            tilemaps[i_index].SetTile(tile.Key, null);
+        }
+    }
+
+    private void Copy_Touching_Layers(int i_excluded_layer)
+    {
+        foreach (var tile in ship_tile_positions)
+        {
+            int index = 0;
+            foreach (var tilemap in tilemaps)
+            {
+                if (index != i_excluded_layer)
+                {
+                    if (tilemaps[index].GetTile(tile.Key) != null)
+                    {
+                        ship_tilemaps[index].SetTile(tile.Key, tilemaps[index].GetTile(tile.Key));
+                        tilemaps[index].SetTile(tile.Key, null);
+                    }
+                }
+
+                index++;
+            }
+        }
+    }
+}
